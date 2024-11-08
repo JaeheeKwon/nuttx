f46f682f003fb05e231ef24ff99606498edc71f4
Jaehee Kwon <jaehee.kwon@gmail.com>
2024-11-07 22:21:49 -0800

    h753

--- boards/arm/stm32h7/nucleo-h753zi/src/nucleo-h753zi.h
+++ boards/arm/stm32h7/nucleo-h753zi/src/nucleo-h753zi.h
@@ -25,8 +25,8 @@
  * Included Files
  ****************************************************************************/
 
-#include <nuttx/config.h>
 #include <nuttx/compiler.h>
+#include <nuttx/config.h>
 
 #include <stdint.h>
 
@@ -36,52 +36,52 @@
 
 /* Configuration ************************************************************/
 
-#define HAVE_PROC            1
-#define HAVE_USBDEV          1
-#define HAVE_USBHOST         1
-#define HAVE_USBMONITOR      1
-#define HAVE_MTDCONFIG       1
+#define HAVE_PROC 1
+#define HAVE_USBDEV 1
+#define HAVE_USBHOST 1
+#define HAVE_USBMONITOR 1
+#define HAVE_MTDCONFIG 1
 #define HAVE_PROGMEM_CHARDEV 1
 
 /* Can't support USB host or device features if USB OTG FS is not enabled */
 
 #ifndef CONFIG_STM32H7_OTGFS
-#  undef HAVE_USBDEV
-#  undef HAVE_USBHOST
+#undef HAVE_USBDEV
+#undef HAVE_USBHOST
 #endif
 
 /* Can't support USB device if USB device is not enabled */
 
 #ifndef CONFIG_USBDEV
-#  undef HAVE_USBDEV
+#undef HAVE_USBDEV
 #endif
 
 /* Can't support USB host is USB host is not enabled */
 
 #ifndef CONFIG_USBHOST
-#  undef HAVE_USBHOST
+#undef HAVE_USBHOST
 #endif
 
 /* Check if we should enable the USB monitor before starting NSH */
 
 #ifndef CONFIG_USBMONITOR
-#  undef HAVE_USBMONITOR
+#undef HAVE_USBMONITOR
 #endif
 
 #ifndef HAVE_USBDEV
-#  undef CONFIG_USBDEV_TRACE
+#undef CONFIG_USBDEV_TRACE
 #endif
 
 #ifndef HAVE_USBHOST
-#  undef CONFIG_USBHOST_TRACE
+#undef CONFIG_USBHOST_TRACE
 #endif
 
 #if !defined(CONFIG_USBDEV_TRACE) && !defined(CONFIG_USBHOST_TRACE)
-#  undef HAVE_USBMONITOR
+#undef HAVE_USBMONITOR
 #endif
 
 #if !defined(CONFIG_STM32H7_PROGMEM) || !defined(CONFIG_MTD_PROGMEM)
-#  undef HAVE_PROGMEM_CHARDEV
+#undef HAVE_PROGMEM_CHARDEV
 #endif
 
 /* This is the on-chip progmem memory driver minor number */
@@ -90,24 +90,24 @@
 
 /* flash  */
 #if defined(CONFIG_MMCSD)
-#  define FLASH_BASED_PARAMS
+#define FLASH_BASED_PARAMS
 #endif
 
 /* procfs File System */
 
 #ifdef CONFIG_FS_PROCFS
-#  ifdef CONFIG_NSH_PROC_MOUNTPOINT
-#    define STM32_PROCFS_MOUNTPOINT CONFIG_NSH_PROC_MOUNTPOINT
-#  else
-#    define STM32_PROCFS_MOUNTPOINT "/proc"
-#  endif
+#ifdef CONFIG_NSH_PROC_MOUNTPOINT
+#define STM32_PROCFS_MOUNTPOINT CONFIG_NSH_PROC_MOUNTPOINT
+#else
+#define STM32_PROCFS_MOUNTPOINT "/proc"
+#endif
 #endif
 
 /* Check if we can support the RTC driver */
 
 #define HAVE_RTC_DRIVER 1
 #if !defined(CONFIG_RTC) || !defined(CONFIG_RTC_DRIVER)
-#  undef HAVE_RTC_DRIVER
+#undef HAVE_RTC_DRIVER
 #endif
 
 /* LED
@@ -117,16 +117,19 @@
  * following definitions assume the default Solder Bridges are installed.
  */
 
-#define GPIO_LD1       (GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_SPEED_50MHz | \
-                        GPIO_OUTPUT_CLEAR | GPIO_PORTB | GPIO_PIN0)
-#define GPIO_LD2       (GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_SPEED_50MHz | \
-                        GPIO_OUTPUT_CLEAR | GPIO_PORTE | GPIO_PIN1)
-#define GPIO_LD3       (GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_SPEED_50MHz | \
-                        GPIO_OUTPUT_CLEAR | GPIO_PORTB | GPIO_PIN14)
+#define GPIO_LD1                                                               \
+  (GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_SPEED_50MHz | GPIO_OUTPUT_CLEAR |        \
+   GPIO_PORTB | GPIO_PIN0)
+#define GPIO_LD2                                                               \
+  (GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_SPEED_50MHz | GPIO_OUTPUT_CLEAR |        \
+   GPIO_PORTE | GPIO_PIN1)
+#define GPIO_LD3                                                               \
+  (GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_SPEED_50MHz | GPIO_OUTPUT_CLEAR |        \
+   GPIO_PORTB | GPIO_PIN14)
 
 #define GPIO_LED_GREEN GPIO_LD1
-#define GPIO_LED_BLUE  GPIO_LD2
-#define GPIO_LED_RED   GPIO_LD3
+#define GPIO_LED_BLUE GPIO_LD2
+#define GPIO_LED_RED GPIO_LD3
 
 #define LED_DRIVER_PATH "/dev/userleds"
 
@@ -141,42 +144,49 @@
  *       installed.
  */
 
-#define GPIO_BTN_USER  (GPIO_INPUT | GPIO_FLOAT | GPIO_EXTI | GPIO_PORTC | GPIO_PIN13)
+#define GPIO_BTN_USER                                                          \
+  (GPIO_INPUT | GPIO_FLOAT | GPIO_EXTI | GPIO_PORTC | GPIO_PIN13)
 
 /* USB OTG FS
  *
  * PA9  OTG_FS_VBUS VBUS sensing (also connected to the green LED)
  * PG6  OTG_FS_PowerSwitchOn
+ * PD10  OTG_FS_PowerSwitchOn
  * PG7  OTG_FS_Overcurrent
  */
 
-#define GPIO_OTGFS_VBUS   (GPIO_INPUT|GPIO_FLOAT|GPIO_SPEED_100MHz| \
-                           GPIO_OPENDRAIN|GPIO_PORTA|GPIO_PIN9)
+#define GPIO_OTGFS_VBUS                                                        \
+  (GPIO_INPUT | GPIO_FLOAT | GPIO_SPEED_100MHz | GPIO_OPENDRAIN | GPIO_PORTA | \
+   GPIO_PIN9)
 
-#define GPIO_OTGFS_PWRON  (GPIO_OUTPUT|GPIO_FLOAT|GPIO_SPEED_100MHz|  \
-                           GPIO_PUSHPULL|GPIO_PORTG|GPIO_PIN6)
+#define GPIO_OTGFS_PWRON                                                       \
+  (GPIO_OUTPUT | GPIO_FLOAT | GPIO_SPEED_100MHz | GPIO_PUSHPULL | GPIO_PORTD | \
+   GPIO_PIN10)
+/* GPIO_PUSHPULL|GPIO_PORTG|GPIO_PIN6) */
 
 #ifdef CONFIG_USBHOST
-#  define GPIO_OTGFS_OVER (GPIO_INPUT|GPIO_EXTI|GPIO_FLOAT| \
-                           GPIO_SPEED_100MHz|GPIO_PUSHPULL| \
-                           GPIO_PORTG|GPIO_PIN7)
+#define GPIO_OTGFS_OVER                                                        \
+  (GPIO_INPUT | GPIO_EXTI | GPIO_FLOAT | GPIO_SPEED_100MHz | GPIO_PUSHPULL |   \
+   GPIO_PORTG | GPIO_PIN7)
 #else
-#  define GPIO_OTGFS_OVER (GPIO_INPUT|GPIO_FLOAT|GPIO_SPEED_100MHz| \
-                           GPIO_PUSHPULL|GPIO_PORTG|GPIO_PIN7)
+#define GPIO_OTGFS_OVER                                                        \
+  (GPIO_INPUT | GPIO_FLOAT | GPIO_SPEED_100MHz | GPIO_PUSHPULL | GPIO_PORTG |  \
+   GPIO_PIN7)
 #endif
 
 /* GPIO pins used by the GPIO Subsystem */
 
-#define BOARD_NGPIOIN     1 /* Amount of GPIO Input pins */
-#define BOARD_NGPIOOUT    1 /* Amount of GPIO Output pins */
-#define BOARD_NGPIOINT    1 /* Amount of GPIO Input w/ Interruption pins */
+#define BOARD_NGPIOIN 1  /* Amount of GPIO Input pins */
+#define BOARD_NGPIOOUT 1 /* Amount of GPIO Output pins */
+#define BOARD_NGPIOINT 1 /* Amount of GPIO Input w/ Interruption pins */
 
 /* Example, used free Ports on the board */
 
-#define GPIO_IN1          (GPIO_INPUT | GPIO_FLOAT | GPIO_PORTE | GPIO_PIN2)
-#define GPIO_OUT1         (GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_SPEED_50MHz | \
-                           GPIO_OUTPUT_SET | GPIO_PORTE | GPIO_PIN4)
-#define GPIO_INT1         (GPIO_INPUT | GPIO_FLOAT | GPIO_PORTE | GPIO_PIN5)
+#define GPIO_IN1 (GPIO_INPUT | GPIO_FLOAT | GPIO_PORTE | GPIO_PIN2)
+#define GPIO_OUT1                                                              \
+  (GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_SPEED_50MHz | GPIO_OUTPUT_SET |          \
+   GPIO_PORTE | GPIO_PIN4)
+#define GPIO_INT1 (GPIO_INPUT | GPIO_FLOAT | GPIO_PORTE | GPIO_PIN5)
 
 /* X-NUCLEO IKS01A2 */
 
@@ -190,21 +200,22 @@
  * IRQ - PD15 (D9)
  */
 
-#define GPIO_NRF24L01_CS   (GPIO_OUTPUT | GPIO_SPEED_50MHz| \
-                            GPIO_OUTPUT_SET | GPIO_PORTA | GPIO_PIN4)
-#define GPIO_NRF24L01_CE   (GPIO_OUTPUT | GPIO_SPEED_50MHz| \
-                            GPIO_OUTPUT_CLEAR | GPIO_PORTF | GPIO_PIN12)
-#define GPIO_NRF24L01_IRQ  (GPIO_INPUT | GPIO_FLOAT | GPIO_PORTD | GPIO_PIN15)
+#define GPIO_NRF24L01_CS                                                       \
+  (GPIO_OUTPUT | GPIO_SPEED_50MHz | GPIO_OUTPUT_SET | GPIO_PORTA | GPIO_PIN4)
+#define GPIO_NRF24L01_CE                                                       \
+  (GPIO_OUTPUT | GPIO_SPEED_50MHz | GPIO_OUTPUT_CLEAR | GPIO_PORTF | GPIO_PIN12)
+#define GPIO_NRF24L01_IRQ (GPIO_INPUT | GPIO_FLOAT | GPIO_PORTD | GPIO_PIN15)
 
 /* MMC/SD
  * CS  - PD15 (D9)
  * NCD - PF12 (D8)
  */
 
-#define GPIO_MMCSD_CS    (GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_SPEED_50MHz | \
-                          GPIO_OUTPUT_SET | GPIO_PORTD | GPIO_PIN15)
-#define GPIO_MMCSD_NCD    (GPIO_INPUT | GPIO_PULLUP | GPIO_EXTI |  \
-                           GPIO_PORTF | GPIO_PIN12)
+#define GPIO_MMCSD_CS                                                          \
+  (GPIO_OUTPUT | GPIO_PUSHPULL | GPIO_SPEED_50MHz | GPIO_OUTPUT_SET |          \
+   GPIO_PORTD | GPIO_PIN15)
+#define GPIO_MMCSD_NCD                                                         \
+  (GPIO_INPUT | GPIO_PULLUP | GPIO_EXTI | GPIO_PORTF | GPIO_PIN12)
 
 /* LMS9DS1 configuration */
 
@@ -212,12 +223,12 @@
 
 /* PCA9635 configuration */
 
-#define PCA9635_I2CBUS  1
+#define PCA9635_I2CBUS 1
 #define PCA9635_I2CADDR 0x40
 
 /* Oled configuration */
 
-#define OLED_I2C_PORT   2
+#define OLED_I2C_PORT 2
 
 /* PWM */
 
